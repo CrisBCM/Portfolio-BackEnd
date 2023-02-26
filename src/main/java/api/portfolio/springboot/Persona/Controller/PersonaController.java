4018c9511c2c0b709e6c936ef715ce229390c1a2
Cristian <cristianbcm1999@gmail.com>
2023-02-26 18:04:36 -0300

    authorization add

--- src/main/java/api/portfolio/springboot/Persona/Controller/PersonaController.java
+++ src/main/java/api/portfolio/springboot/Persona/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import api.portfolio.springboot.Persona.Entity.Persona;
 import api.portfolio.springboot.Persona.Service.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +21,7 @@ public class PersonaController {
     @Autowired
     private IPersonaService personaService;
     
-    
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/crear/persona")
     public String crearPersona(@RequestBody Persona pers){
         personaService.crearPersona(pers);
@@ -38,7 +39,7 @@ public class PersonaController {
        return personaService.traerPersonaPorId(id);
     }
     
-
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/eliminar/persona/{id}")
     public String eliminarPersona(@PathVariable int id){
         personaService.EliminarPersona(id);
@@ -46,6 +47,7 @@ public class PersonaController {
         return "Persona eliminada correctamente!";
     }
    
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/editar/persona/{id}")
     public String editarPersona(@PathVariable int id,
                                 @RequestParam ("nombre") String nuevoNombre,
